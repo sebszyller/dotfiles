502da2c4269c882c3371c126840747a43222343c
Sebastian Szyller <github@sebszyller.com>
2022-04-15 16:51:06 +0300

    Cleaned up git log aliases.

--- alias/git.sh
+++ alias/git.sh
@@ -1,3 +1,5 @@
+local fmt='--pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"'
+
 alias gs="git status"
 alias ga="git add"
 alias gb="git branch"
@@ -7,9 +9,11 @@ alias pchaj="git push"
 alias gclone="git clone"
 alias gpull="git pull"
 alias gcm="git commit -m"
-alias gl='clear; git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"'
-alias gls='clear; git log --stat --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"'
-alias glp='clear; git log -p --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"'
+alias gl="clear; git log --oneline $fmt"
+alias glg="clear; git log --graph $fmt"
+alias glp="clear; git log -p $fmt"
+alias gls="clear; git log --stat $fmt"
+alias gsh="clear; git show $fmt"
 alias gr="git reset"
 alias grs="git reset --soft"
 alias grh="git reset --hard"
@@ -22,3 +26,9 @@ alias gmerge="gmerge"
 alias gdiff="git diff"
 alias gdiffc="git diff --cached"
 alias gcl="git clean"
+
+gsf() {
+    clear
+    local sha=$(eval "git log --oneline $fmt --color=always" | fzf --ansi --no-sort | awk '{print $1}')
+    gsh --color=always --decorate $sha
+}
