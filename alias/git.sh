6f2a67fcb1e02370b49ef36c60dc0cef282f7194
Sebastian Szyller <sebastian.szyller@aalto.fi>
2022-07-14 16:49:44 +0300

    Removed rarely used git aliases.

--- alias/git.sh
+++ alias/git.sh
@@ -4,30 +4,26 @@ alias ga="git add"
 alias gb="git branch"
 alias gc="git checkout"
 alias gcl="git clean"
-alias gclone="git clone"
 alias gcm="git commit -m"
 alias gdf="git diff"
 alias gdfc="git diff --cached"
-alias gfetch="git fetch"
 alias gl="clear; git log --oneline $fmt"
 alias glg="clear; git log --graph $fmt"
 alias glp="clear; git log -p $fmt"
 alias gls="clear; git log --stat $fmt"
-alias gmerge="gmerge"
 alias gpull="git pull"
 alias gpush="git push"
 alias pchaj="git push"
 alias gr="git reset"
-alias grebase="git rebase"
 alias grh="git reset --hard"
 alias grs="git reset --soft"
 alias gru="git remote update"
 alias gs="git status"
-alias gsh="clear; git show $fmt"
 alias gst="git stash"
 alias gsta="git stash apply"
+alias gti=git
 
-gshf() {
+gsh() {
     clear
     local sha=$(eval "git log --oneline $fmt --color=always" | fzf --ansi --no-sort | awk '{print $1}')
     gsh --color=always --decorate $sha
