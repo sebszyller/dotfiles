444bb919a91ec669ebad7c3b3e5f934e166c9de1
Sebastian Szyller <github@sebszyller.com>
2024-01-21 19:09:17 +0200

    Added split remaps. Cleaned up setting files.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -42,6 +42,8 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
 vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
 vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
 vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", opts)
+vim.keymap.set("n", "<leader>sh", ":split<CR>", opts)
 
 -- Pane resize
 vim.keymap.set('n', '<S-Up>', ':resize -2<CR>', opts)
@@ -59,4 +61,3 @@ vim.keymap.set("n", "<leader>q", ":q<CR>", opts)
 vim.keymap.set("n", "<leader>wq", ":wq<CR>", opts)
 vim.keymap.set("n", "<leader>w", ":w<CR>", opts)
 vim.keymap.set("n", "<leader>wr", ":set wrap!<CR>", opts)
-
