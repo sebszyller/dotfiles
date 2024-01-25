537698b9bdbd66881d0d0e9cc77bd59a5854b20c
Sebastian Szyller <github@sebszyller.com>
2024-01-25 17:02:10 +0200

    Changed nvim line number colour.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,13 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            local colors = require("ayu.colors")
+            colors.generate()
+            require("ayu").setup({
+                overrides = {
+                    LineNr = { fg = colors.comment }
+                }
+            })
             vim.cmd([[colorscheme ayu]])
         end,
     },
