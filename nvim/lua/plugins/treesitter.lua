b49bc0fc68209adca5219c64d8c02da404ab2f50
Sebastian Szyller <github@sebszyller.com>
2024-02-07 22:33:57 +0200

    Updated tmux.conf.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,26 +5,25 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
-    local configs = require("nvim-treesitter.configs")
+      local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-        ensure_installed = {
-            "bash", "fish",
-            "html", "css", "scss",
-            "javascript", "typescript",
-            "json", "yaml", "toml", "csv",
-            "lua",
-            "latex", "markdown", "markdown_inline",
-            "python",
-            "regex",
-            "rust",
-            "vim"
-        },
-        sync_install = false,
-        auto_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-        additional_vim_regex_highlighting = { "markdown", "markdown_inline" }
-    })
+      configs.setup({
+          ensure_installed = {
+                "bash",
+                "html", "css", "scss",
+                "javascript", "typescript",
+                "json", "yaml", "toml", "csv",
+                "lua",
+                "markdown", "markdown_inline",
+                "python",
+                "regex",
+                "rust",
+                "vim"
+          },
+          sync_install = false,
+          auto_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
     end
 }
