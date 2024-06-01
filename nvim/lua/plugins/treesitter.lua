5ae982085eeed28516dabb7c28a357c49333a360
Sebastian Szyller <github@sebszyller.com>
2024-06-01 20:27:02 +0300

    feat: added treesitter text objects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,30 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	tag = "v0.9.2",
-	dependencies = { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	dependencies = {
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		{ "nvim-treesitter/nvim-treesitter-textobjects", commit = "fd41b7ccc5490a3a99c734d1ee418b68d06c48a9" },
+	},
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["if"] = "@function.inner",
+						["af"] = "@function.outer",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["ii"] = "@conditional.inner",
+						["ai"] = "@conditional.outer",
+					},
+				},
+			},
 			ensure_installed = {
 				"bash",
 				"fish",
