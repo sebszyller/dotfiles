1a4b9c8e5cc9f7cabb0269609eedc50f9c8fa831
Sebastian Szyller <github@sebszyller.com>
2024-06-26 21:18:25 +0300

    chore: bumped obsidian.nvim version that fixes cmp dupes

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,6 +1,6 @@
 return {
 	"epwalsh/obsidian.nvim",
-	tag = "v3.7.14",
+	tag = "v3.8.1",
 	lazy = true,
 	event = {
 		"BufReadPre " .. vim.fn.expand("~") .. "/obsidian/**.md",
@@ -18,7 +18,16 @@ return {
 				reference_text = {},
 				external_link_icon = { char = "" },
 			},
-			wiki_link_func = "use_path_only",
+			wiki_link_func = function(opts)
+				---@type string
+				local header_or_block = ""
+				if opts.anchor then
+					header_or_block = opts.anchor.anchor
+				elseif opts.block then
+					header_or_block = string.format("#%s", opts.block.id)
+				end
+				return string.format("[[%s%s]]", opts.id, header_or_block)
+			end,
 			-- hl_groups = {
 			--     ObsidianBullet = { bold = true, fg = palette.faded_orange },
 			-- }
