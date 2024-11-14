e0ddd9d91016305ef67675252b397898f8873ee4
Sebastian Szyller <github@sebszyller.com>
2024-11-14 14:12:24 +0200

    fix: don't start new ssh-agent for every shell

--- fish/config.fish
+++ fish/config.fish
@@ -35,7 +35,37 @@ set -gx MANPAGER 'sh -c "col -bx | bat -l man --theme=\'Solarized (dark)\' -p"'
 bind \cx "clear-screen"
 
 # Init agent
-eval (ssh-agent -c) >/dev/null
+function init_ssh_agent
+    # https://rabexc.org/posts/pitfalls-of-ssh-agents
+    # https://stackoverflow.com/questions/40549332/how-to-check-if-ssh-agent-is-already-running-in-bash
+    ssh-add -l &>/dev/null
+    if test $status -eq 2
+        # Could not open a connection to your authentication agent.
+        # Load stored agent connection info.
+        if test -r ~/.ssh-agent
+            eval (cat ~/.ssh-agent) >/dev/null
+        end
+
+        ssh-add -l &>/dev/null
+        if test $status -eq 2
+            # Start agent and store agent connection info.
+            begin
+                umask 066
+                ssh-agent -c > ~/.ssh-agent
+            end
+            eval (cat ~/.ssh-agent) >/dev/null
+        end
+    end
+
+    # Load identities
+    ssh-add -l &>/dev/null
+    if test $status -eq 1
+        # The agent has no identities.
+        # Time to add one.
+        ssh-add -t 8h &>/dev/null
+    end
+end
+init_ssh_agent
 
 # Source functions
 source $FISH/functions/git.fish
