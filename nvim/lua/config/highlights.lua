ac3f5dbefe81395c7125bfb22f0e7b3bc898fdc3
Sebastian Szyller <github@sebszyller.com>
2025-01-22 19:50:12 +0200

    feat: tweak and simplify palette

--- nvim/lua/config/highlights.lua
+++ nvim/lua/config/highlights.lua
@@ -2,23 +2,23 @@ local hl = vim.api.nvim_set_hl
 local palette = require("zenbones.palette")
 local blue = palette.dark.water.hex
 local bg = palette.dark.bg.hex
-local green = palette.dark.leaf.hex
-local cyan = palette.dark.sky.hex
+local yellow = "#f6c177"
 
 -- LSP
 hl(0, "LspInlayHint", { link = "Comment" })
-hl(0, "Type", { fg = green })
-hl(0, "Constant", { fg = blue, italic = true })
+hl(0, "Type", { fg = blue })
+hl(0, "Constant", { fg = yellow, italic = true })
 
 -- Markdown
 hl(0, "@markup.link.label.markdown_inline", { fg = blue, underline = true })
 hl(0, "@markup.raw.markdown_inline", { link = "Comment" })
 for i = 1, 6 do
-	hl(0, "@markup.heading." .. i .. ".markdown", { fg = green, bold = true })
+	hl(0, "@markup.heading." .. i .. ".markdown", { fg = yellow, bold = true })
 end
 
 -- UI
-hl(0, "TelescopeSelection", { link = "Visual" })
+hl(0, "TelescopeSelection", { fg = yellow, bg = bg })
+hl(0, "QuickFixLine", { fg = yellow, bg = bg })
 
 hl(0, "NormalFloat", { bg = "none" })
 hl(0, "FloatBorder", { bg = "none" })
@@ -28,7 +28,7 @@ hl(0, "TelescopeBorder", { bg = "none" })
 -- CMP
 hl(0, "Pmenu", { bg = "none" })
 hl(0, "PmenuSbar", { bg = "none" })
-hl(0, "PmenuSel", { fg = cyan, bg = bg })
+hl(0, "PmenuSel", { fg = yellow, bg = bg })
 
 hl(0, "PmenuThumb", { bg = "none" })
 hl(0, "BlinkCmpLabel", { link = "Identifier" })
