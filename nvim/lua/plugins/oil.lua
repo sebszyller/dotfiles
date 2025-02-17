eaf6ed0643b18789cb6617b059050a69bb0c939e
Sebastian Szyller <contact@sebszyller.com>
2025-02-17 14:07:21 +0200

    feat: yeet netrw

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,50 +1,31 @@
 return {
-	"stevearc/oil.nvim",
-	version = "2.12.2",
-	lazy = false,
-	opts = {},
-	config = function()
-		require("oil").setup({
-			columns = { "icon" },
-			default_file_explorer = false,
-			keymaps = {
-				["<C-h>"] = false,
-				["<C-l>"] = false,
-				["<C-p>"] = {
-					callback = function()
-						local oil = require("oil")
-						oil.open_preview({ vertical = true, split = "botright" })
-					end,
-				},
-			},
-			skip_confirm_for_simple_edits = true,
-			view_options = {
-				show_hidden = true,
-			},
-			win_options = {
-				signcolumn = "yes",
-			},
-		})
+    "stevearc/oil.nvim",
+    version = "2.15.0",
+    lazy = false,
+    config = function()
+        require("oil").setup({
+            columns = { "icon" },
+            default_file_explorer = false,
+            keymaps = {
+                ["<C-h>"] = false,
+                ["<C-l>"] = false,
+                ["<C-p>"] = {
+                    callback = function()
+                        local oil = require("oil")
+                        oil.open_preview({ vertical = true, split = "botright" })
+                    end,
+                },
+            },
+            skip_confirm_for_simple_edits = true,
+            view_options = {
+                show_hidden = true,
+            },
+            win_options = {
+                signcolumn = "yes",
+            },
+        })
 
-		local function toggle_netrw()
-			if vim.bo.filetype == "netrw" then
-				vim.cmd("Rex")
-			else
-				vim.cmd("Ex")
-			end
-		end
-
-		local opts = { noremap = true, silent = true }
-        -- stylua: ignore start
-		_map("n", "-", "<CMD>Oil<CR>", { desc = "Oil", unpack(opts) })
-		_map("n", "<leader>-", function() toggle_netrw() end, { desc = "Netrw", unpack(opts) })
-		-- stylua: ignore end
-		vim.g.netrw_altv = 1
-		vim.g.netrw_banner = 0
-		vim.g.netrw_keepdir = 1
-		vim.g.netrw_indent = 4
-		vim.g.netrw_list_hide = ""
-		vim.g.netrw_liststyle = 3
-		vim.g.netrw_preview = 1
-	end,
+        -- stylua: ignore
+        _map("n", "-", "<CMD>Oil<CR>", { desc = "Oil", noremap = true, silent = true })
+    end,
 }
