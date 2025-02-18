92eaa83c04d432d70bf334f6d2c8493c8706dccc
Sebastian Szyller <contact@sebszyller.com>
2025-02-18 15:15:22 +0200

    feat: change telscope pointer; reduce ui contrast

--- nvim/lua/config/highlights.lua
+++ nvim/lua/config/highlights.lua
@@ -1,23 +1,27 @@
 local hl = vim.api.nvim_set_hl
 local palette = require("zenbones.palette")
-local blue = palette.dark.water.hex
 local bg = palette.dark.bg.hex
-local yellow = "#f6c177"
+local blue = "#7788aa"
+local green = "#789978"
+local yellow = "#a1938c"
 
 -- LSP
 hl(0, "LspInlayHint", { link = "Comment" })
-hl(0, "Type", { fg = blue })
-hl(0, "Constant", { fg = yellow, italic = true })
+hl(0, "Type", { fg = green })
+hl(0, "Constant", { italic = true })
 
--- Markdown
 hl(0, "@markup.link.label.markdown_inline", { fg = blue, underline = true })
 hl(0, "@markup.raw.markdown_inline", { link = "Comment" })
 for i = 1, 6 do
-	hl(0, "@markup.heading." .. i .. ".markdown", { fg = yellow, bold = true })
+    hl(0, "@markup.heading." .. i .. ".markdown", { fg = green, bold = true })
 end
+
+hl(0, "@module.latex", { fg = green })
+hl(0, "@string", { fg = blue, italic = true })
 
 -- UI
 hl(0, "TelescopeSelection", { fg = yellow, bg = bg })
+hl(0, "TelescopeSelectionCaret", { fg = yellow })
 hl(0, "QuickFixLine", { fg = yellow, bg = bg })
 
 hl(0, "NormalFloat", { bg = "none" })
@@ -25,7 +29,6 @@ hl(0, "FloatBorder", { bg = "none" })
 hl(0, "FloatTitle", { bg = "none" })
 hl(0, "TelescopeBorder", { bg = "none" })
 
--- CMP
 hl(0, "Pmenu", { bg = "none" })
 hl(0, "PmenuSbar", { bg = "none" })
 hl(0, "PmenuSel", { fg = yellow, bg = bg })
