8eb66292681c9aee87eb692b05adebc567b0c058
Sebastian Szyller <contact@sebszyller.com>
2025-02-18 15:15:22 +0200

    feat: change telscope pointer; reduce ui contrast

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,14 @@
 return {
-	"zenbones-theme/zenbones.nvim",
-	commit = "006f15ad3437d85884ad50347bcbc60c9a377640",
-	dependencies = {
-		{ "rktjmp/lush.nvim", version = "1b66ab197b177aabc2270a2c9cb8019fa5a367df" },
-	},
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.o.background = "dark"
-		vim.g.zenbones = { transparent_background = true }
-		vim.cmd("colorscheme zenbones")
-	end,
+    "zenbones-theme/zenbones.nvim",
+    commit = "006f15ad3437d85884ad50347bcbc60c9a377640",
+    dependencies = {
+        { "rktjmp/lush.nvim", version = "1b66ab197b177aabc2270a2c9cb8019fa5a367df" },
+    },
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.o.background = "dark"
+        vim.g.zenbones = { transparent_background = true }
+        vim.cmd("colorscheme zenbones")
+    end,
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,88 +1,89 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			commit = "dae2eac9d91464448b584c7949a31df8faefec56",
-			build = "make",
-		},
-		{ "nvim-lua/plenary.nvim", commit = "2d9b06177a975543726ce5c73fca176cedbffe9d" },
-	},
-	config = function()
-		local builtin = require("telescope.builtin")
-		local telescope = require("telescope")
-		require("telescope.pickers.layout_strategies").horizontal_merged = function(
-			picker,
-			max_columns,
-			max_lines,
-			layout_config
-		)
-			local layout =
-				require("telescope.pickers.layout_strategies").horizontal(picker, max_columns, max_lines, layout_config)
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            commit = "dae2eac9d91464448b584c7949a31df8faefec56",
+            build = "make",
+        },
+        { "nvim-lua/plenary.nvim", commit = "2d9b06177a975543726ce5c73fca176cedbffe9d" },
+    },
+    config = function()
+        local builtin = require("telescope.builtin")
+        local telescope = require("telescope")
+        require("telescope.pickers.layout_strategies").horizontal_merged = function(
+            picker,
+            max_columns,
+            max_lines,
+            layout_config
+        )
+            local layout =
+                require("telescope.pickers.layout_strategies").horizontal(picker, max_columns, max_lines, layout_config)
 
-			layout.prompt.title = ""
-			layout.prompt.borderchars = { "─", "│", "─", "│", "┌", "┬", "┤", "├" }
+            layout.prompt.title = ""
+            layout.prompt.borderchars = { "─", "│", "─", "│", "┌", "┬", "┤", "├" }
 
-			layout.results.title = ""
-			layout.results.borderchars = { "─", "│", "─", "│", "├", "┤", "┴", "└" }
-			layout.results.line = layout.results.line - 1
-			layout.results.height = layout.results.height + 1
+            layout.results.title = ""
+            layout.results.borderchars = { "─", "│", "─", "│", "├", "┤", "┴", "└" }
+            layout.results.line = layout.results.line - 1
+            layout.results.height = layout.results.height + 1
 
-			layout.preview.title = ""
-			layout.preview.borderchars = { "─", "│", "─", " ", "─", "┐", "┘", "─" }
+            layout.preview.title = ""
+            layout.preview.borderchars = { "─", "│", "─", " ", "─", "┐", "┘", "─" }
 
-			return layout
-		end
-		require("telescope.pickers.layout_strategies").vertical_merged = function(
-			picker,
-			max_columns,
-			max_lines,
-			layout_config
-		)
-			local layout =
-				require("telescope.pickers.layout_strategies").vertical(picker, max_columns, max_lines, layout_config)
+            return layout
+        end
+        require("telescope.pickers.layout_strategies").vertical_merged = function(
+            picker,
+            max_columns,
+            max_lines,
+            layout_config
+        )
+            local layout =
+                require("telescope.pickers.layout_strategies").vertical(picker, max_columns, max_lines, layout_config)
 
-			layout.prompt.title = ""
-			layout.prompt.borderchars = { "─", "│", "─", "│", "┌", "┐", "┤", "├" }
+            layout.prompt.title = ""
+            layout.prompt.borderchars = { "─", "│", "─", "│", "┌", "┐", "┤", "├" }
 
-			layout.results.title = ""
-			layout.results.borderchars = { "─", "│", "─", "│", "├", "┤", "┘", "└" }
-			layout.results.line = layout.results.line - 1
-			layout.results.height = layout.results.height + 1
+            layout.results.title = ""
+            layout.results.borderchars = { "─", "│", "─", "│", "├", "┤", "┘", "└" }
+            layout.results.line = layout.results.line - 1
+            layout.results.height = layout.results.height + 1
 
-			return layout
-		end
-		telescope.setup({
-			defaults = {
-				get_status_text = function(picker)
-					return ""
-				end,
-				layout_strategy = "horizontal_merged",
-				layout_config = {
-					horizontal = {
-						prompt_position = "top",
-						preview_width = 0.6,
-						height = 0.8,
-					},
-				},
-				sorting_strategy = "ascending",
-			},
-			pickers = {
-				command_history = {
-					layout_strategy = "vertical_merged",
-					layout_config = {
-						height = 0.5,
-						prompt_position = "top",
-					},
-				},
-			},
-		})
-		local opts = { noremap = true, silent = true }
+            return layout
+        end
+        telescope.setup({
+            defaults = {
+                get_status_text = function(picker)
+                    return ""
+                end,
+                layout_strategy = "horizontal_merged",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        preview_width = 0.6,
+                        height = 0.8,
+                    },
+                },
+                selection_caret = "❚ ",
+                sorting_strategy = "ascending",
+            },
+            pickers = {
+                command_history = {
+                    layout_strategy = "vertical_merged",
+                    layout_config = {
+                        height = 0.5,
+                        prompt_position = "top",
+                    },
+                },
+            },
+        })
+        local opts = { noremap = true, silent = true }
         -- stylua: ignore start
         _map("n", "<leader>fc", builtin.command_history, { desc = "Command history", unpack(opts) })
         _map("n", "<leader>fd", builtin.find_files, { desc = "Find files", unpack(opts) })
         _map("n", "<leader>fg", builtin.live_grep, { desc = "Grep in files", unpack(opts) })
-		-- stylua: ignore start
-	end,
+        -- stylua: ignore start
+    end,
 }
