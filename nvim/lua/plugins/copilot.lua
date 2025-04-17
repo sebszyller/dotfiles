41e80fc58f72a6e6a304d487046b218e951e70e4
Sebastian Szyller <contact@sebszyller.com>
2025-04-17 11:07:47 +0300

    chore: update copilot model to 4o

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,12 +1,13 @@
 return {
     "zbirenbaum/copilot.lua",
-    commit = "228cd7456fd923b8da3eb6497bb345de58b93637",
+    commit = "0b435497295f01e253f3c1777e02f4553da7f59d",
     event = "LspAttach",
     dependencies = {
         { "giuxtaposition/blink-cmp-copilot", commit = "c5c5cbce5748d21073f1d5348a92ebe6ce63f387" },
     },
     cmd = "Copilot",
     opts = {
+        copilot_model = "gpt-4o-copilot",
         panel = { enabled = false },
         suggestion = { enabled = false },
         filetypes = {
