b15193338c0dbd036157cd602edb698548ef146f
Sebastian Szyller <contact@sebszyller.com>
2025-08-31 16:36:23 +0300

    chore: remove bqf

--- stow/.config/nvim/lua/plugins/quickfix.lua
+++ stow/.config/nvim/lua/plugins/quickfix.lua
@@ -5,17 +5,4 @@ return {
         event = "FileType qf",
         opts = {},
     },
-    {
-        "kevinhwang91/nvim-bqf",
-        version = "1.1.1",
-        event = "FileType qf",
-        opts = {
-            delay_syntax = 10,
-            preview = {
-                buf_label = false,
-                show_scrollbar = false,
-                winblend = 0,
-            },
-        },
-    },
 }
