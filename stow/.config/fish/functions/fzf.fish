4828f45c26865070bc5734f7f6ade5620ec9303b
Sebastian Szyller <contact@sebszyller.com>
2025-11-25 20:07:23 +0200

    fix: disable fzf gutter

--- stow/.config/fish/functions/fzf.fish
+++ stow/.config/fish/functions/fzf.fish
@@ -3,7 +3,8 @@ set -gx FZF_DEFAULT_OPTS "
     --color=fg+:#b4bdc3,bg+:#3d4042,hl+:#a1938c
     --color=info:#6e6763,prompt:#b4bdc3,pointer:#a1938c
     --color=marker:#6099c0,spinner:#b279a7,header:#6099c0
-    --color=separator:#6e6763,border:#6e6763,gutter:-1
+    --color=separator:#6e6763,border:#6e6763
+    --gutter=' '
     --height 40%  --no-scrollbar
     --prompt='> ' --pointer='❚' --layout=reverse
     --border=rounded"
