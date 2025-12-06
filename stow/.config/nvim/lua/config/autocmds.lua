0506053fde507684c8ad4491bfc7231db5377833
Sebastian Szyller <contact@sebszyller.com>
2025-12-06 15:00:02 +0200

    fix: update obsidian backlinks position on save

--- stow/.config/nvim/lua/config/autocmds.lua
+++ stow/.config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,7 @@ local autocmd = vim.api.nvim_create_autocmd
 local default_group = vim.api.nvim_create_augroup("DefaultGroup", { clear = true })
 local copilot_group = vim.api.nvim_create_augroup("CopilotFocus", { clear = true })
 local lsp_group = vim.api.nvim_create_augroup("LspGroup", { clear = true })
+local obsidian_group = vim.api.nvim_create_augroup("ObsidianGroup", { clear = true })
 local oil_group = vim.api.nvim_create_augroup("OilGroup", { clear = true })
 
 autocmd("BufWritePre", {
@@ -93,6 +94,70 @@ autocmd("User", {
 
         vim.api.nvim_buf_clear_namespace(0, ns_id, 0, -1)
         vim.api.nvim_buf_set_extmark(0, ns_id, 0, 0, opts)
+    end,
+})
+
+---@param note obsidian.Note
+---@return string[]
+local function unique_backlinks(note)
+    local Note = require("obsidian.note")
+    local backlinks = note:backlinks()
+    local titles = {}
+    local seen = {}
+
+    for _, file_table in ipairs(backlinks) do
+        local t = Note.from_file(file_table.path.filename).title
+        if not seen[t] then
+            titles[#titles + 1] = t
+            seen[t] = true
+        end
+    end
+
+    return titles
+end
+
+autocmd("User", {
+    group = obsidian_group,
+    pattern = { "ObsidianNoteEnter", "ObsidianNoteWritePost" },
+    callback = function(args)
+        local bufnr = args.buf
+        local note = require("obsidian.note").from_buffer(bufr)
+        local hl = "Comment"
+        local line_count = vim.api.nvim_buf_line_count(bufnr)
+        local ns_id = vim.api.nvim_create_namespace("ObsidianVirtualBacklinks")
+
+        vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
+
+        local virt_lines = {}
+        local backlinks = unique_backlinks(note)
+        if next(backlinks) == nil then
+            table.insert(virt_lines, { { "[No backlinks]", hl } })
+        else
+            table.insert(virt_lines, { { "[Backlinks]", hl } })
+            table.sort(backlinks)
+            for _, title in ipairs(backlinks) do
+                table.insert(virt_lines, { { title, hl } })
+            end
+        end
+
+        vim.api.nvim_buf_set_extmark(bufnr, ns_id, line_count - 1, 0, {
+            virt_lines = virt_lines,
+            virt_lines_above = false,
+        })
+    end,
+})
+
+autocmd("User", {
+    group = obsidian_group,
+    pattern = "ObsidianNoteEnter",
+    callback = function(args)
+        local opts = { buffer = args.bufnr, noremap = false, silent = true }
+        -- stylua: ignore start
+        Globals.map("n", "gd",         ":Obsidian follow_link<CR>",        Globals.extend(opts, { desc = "Follow link" }))
+        Globals.map("n", "gD",         ":Obsidian follow_link vsplit<CR>", Globals.extend(opts, { desc = "Follow link (split)" }))
+        Globals.map("n", "<leader>ob", ":Obsidian backlinks<CR>",          Globals.extend(opts, { desc = "Backlinks" }))
+        Globals.map("n", "<leader>on", ":Obsidian new<CR>",                Globals.extend(opts, { desc = "New note" }))
+        -- stylua: ignore end
     end,
 })
 
