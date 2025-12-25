58859f4e08d11ecb86dbeff24a756676b06c12e5
Sebastian Szyller <contact@sebszyller.com>
2025-12-25 15:13:09 +0200

    feat: add QuickMake

---
+++ stow/.config/nvim/lua/plugins/make.lua
@@ -0,0 +1,8 @@
+return {
+    "sebszyller/quick-make.nvim",
+    commit = "2c67c1d8f3ed875391a7a99cd2e6602b2c750bde",
+    keys = {
+        { "<leader>m", ":QuickMake<CR>", desc = "QuickMake" },
+    },
+    opts = {},
+}
