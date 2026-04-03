d0ad1ba2f947c2aa4fa3c07e3669f4051fb9bd93
Sebastian Szyller <contact@sebszyller.com>
2026-04-03 15:05:56 +0300

    feat: custom git-split

--- stow/.config/fish/functions/git.fish
+++ stow/.config/fish/functions/git.fish
@@ -10,9 +10,9 @@ alias gcmax "git commit --amend --no-edit"
 alias gdf "git diff"
 alias gdfc "git diff --cached"
 alias glp "clear; git log -p --show-signature $__gitfmt"
-alias gls "clear; git log --stat --show-signature $__gitfmt"
 alias gpull "git pull"
 alias gpush "git push"
+alias gpushf "git push --force-with-lease"
 alias gr "git reset"
 alias grh "git reset --hard"
 alias grs "git reset --soft"
@@ -28,7 +28,7 @@ alias wt "git worktree"
 
 function gl
     clear
-    set --local sha (eval "git log --oneline $__gitfmt --color=always" $argv[1] | fzf --header "<CR>: select | <C-c>: abort" --ansi --no-sort --height=80% --preview="git show --show-signature --color=always $__gitfmt --stat {1}" | awk '{print $1}')
+    set --local sha (eval "git log --oneline $__gitfmt --color=always" $argv[1] | fzf --header "<CR>: select | <C-c>: abort | <C-y>: yank sha" --bind="ctrl-y:execute-silent(echo -n {1} | yy)+transform-header(echo '<CR>: select | <C-c>: abort | <C-y>: yank sha (last yank: '{1}')')" --ansi --no-sort --height=80% --preview="git show --show-signature --color=always $__gitfmt --stat {1}" | awk '{print $1}')
 
     if test -z "$sha"
         kill -INT $fish_pid
