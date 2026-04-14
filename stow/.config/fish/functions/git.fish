d089bac929d242c3099a1d0ef64121c41b25b4ff
Sebastian Szyller <contact@sebszyller.com>
2026-04-14 22:36:24 +0300

    fix: git log formatting for --graph

--- stow/.config/fish/functions/git.fish
+++ stow/.config/fish/functions/git.fish
@@ -1,4 +1,4 @@
-set --global __gitfmt '--pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"'
+set --global __gitfmt '--pretty=format:"%C(red)%h%Creset %s %C(green)(%cr) %C(dim white)%an%Creset%C(#D4A85C)%d%C(reset)"'
 
 alias ga "git add"
 alias gap "git add -p"
@@ -10,6 +10,7 @@ alias gcmax "git commit --amend --no-edit"
 alias gdf "git diff"
 alias gdfc "git diff --cached"
 alias glp "clear; git log -p --show-signature $__gitfmt"
+alias gla "gl --all"
 alias gpull "git pull"
 alias gpush "git push"
 alias gpushf "git push --force-with-lease"
@@ -28,14 +29,21 @@ alias wt "git worktree"
 
 function gl
     clear
-    set --local sha (eval "git log --oneline $__gitfmt --color=always" $argv[1] | fzf --header "<CR>: select | <C-c>: abort | <C-y>: yank sha" --bind="ctrl-y:execute-silent(echo -n {1} | yy)+transform-header(echo '<CR>: select | <C-c>: abort | <C-y>: yank sha (last yank: '{1}')')" --ansi --no-sort --height=80% --preview="git show --show-signature --color=always $__gitfmt --stat {1}" | awk '{print $1}')
+    set --local sha (eval "git log --graph --oneline $__gitfmt --color=always" $argv[1] | fzf \
+        --header "<CR>: select | <C-c>: abort | <C-y>: yank sha" \
+        --bind="ctrl-y:execute-silent(echo {} | rg -o '[a-f0-9]{7,}' | head -n 1 | tr -d '\n' | yy)+transform-header(echo {} | rg -o '[a-f0-9]{7,}' | head -n 1 | xargs -I % echo '<CR>: select | <C-c>: abort | <C-y>: yank sha (last yank: %)')" \
+        --ansi --no-sort --height=80% \
+        --preview="set -l commit (echo {} | rg -o '[a-f0-9]{7,}' | head -n 1); if test -n \"\$commit\"; git show --show-signature --color=always $__gitfmt --stat \$commit; else; echo 'Select a commit node to preview...'; end" \
+        | rg -o '[a-f0-9]{7,}' | head -n 1)
 
     if test -z "$sha"
         kill -INT $fish_pid
     else
-        git show --show-signature $__gitfmt --show-signature --color=always --decorate $sha
+        git show --show-signature $__gitfmt --color=always --decorate $sha
     end
 end
+
+complete --command gl --wraps 'git log'
 
 function wtl
     # List and kill with <C-x>
