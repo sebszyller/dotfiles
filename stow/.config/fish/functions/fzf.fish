5058b8c5a9ab69e2f28bd786d1f667c0615b2d09
Sebastian Szyller <contact@sebszyller.com>
2026-06-19 13:44:41 +0300

    chore: simplify zenbones highlights

--- stow/.config/fish/functions/fzf.fish
+++ stow/.config/fish/functions/fzf.fish
@@ -1,7 +1,7 @@
 set -gx FZF_DEFAULT_OPTS "
-    --color=fg:#b4bdc3,bg:-1,hl:#a1938c
-    --color=fg+:#b4bdc3,bg+:#3d4042,hl+:#a1938c
-    --color=info:#6e6763,prompt:#b4bdc3,pointer:#a1938c
+    --color=fg:#b4bdc3,bg:-1,hl:#d4a85c
+    --color=fg+:#d4a85c,bg+:#1c1917,hl+:#d4a85c
+    --color=info:#6e6763,prompt:#b4bdc3,pointer:#d4a85c
     --color=marker:#6099c0,spinner:#b279a7,header:#6099c0
     --color=separator:#6e6763,border:#6e6763
     --gutter=' '
--- stow/.config/nvim/lua/config/highlights.lua
+++ stow/.config/nvim/lua/config/highlights.lua
@@ -1,20 +1,20 @@
 local hl = vim.api.nvim_set_hl
-local palette = require("zenbones.palette")
-local bg = palette.dark.bg.hex
-local blue = palette.dark.water.hex
-local green = palette.dark.leaf.hex
-local magenta = palette.dark.blossom.hex
-local yellow = "#a1938c"
+
+local palette = require("zenbones.palette").dark
+local bg = palette.bg.hex
+local blue = palette.water.hex
+local green = palette.leaf.hex
+local magenta = palette.blossom.hex
+local yellow = "#d4a85c"
+
+local selection = { fg = yellow, bg = bg }
 
 -- LSP
 hl(0, "LspInlayHint", { link = "Comment" })
 hl(0, "Type", { fg = green })
 
 hl(0, "@markup.link.label.markdown_inline", { fg = blue, underline = true })
-hl(0, "@markup.raw.markdown_inline", { link = "Comment" })
-for i = 1, 6 do
-    hl(0, "@markup.heading." .. i .. ".markdown", { fg = green, bold = true })
-end
+hl(0, "@markup.raw.markdown_inline", { fg = magenta, italic = true })
 
 hl(0, "typescriptTemplate", { link = "@string" })
 hl(0, "typescriptString", { link = "@string" })
@@ -27,9 +27,9 @@ hl(0, "ObsidianBullet", { fg = green })
 hl(0, "@string", { fg = blue })
 
 -- UI
-hl(0, "TelescopeSelection", { fg = yellow, bg = bg })
-hl(0, "TelescopeSelectionCaret", { fg = yellow })
-hl(0, "QuickFixLine", { fg = yellow, bg = bg })
+hl(0, "TelescopeSelection", selection)
+hl(0, "TelescopeSelectionCaret", selection)
+hl(0, "QuickFixLine", selection)
 
 hl(0, "NormalFloat", { bg = "none" })
 hl(0, "FloatBorder", { bg = "none" })
@@ -38,9 +38,9 @@ hl(0, "TelescopeBorder", { bg = "none" })
 
 hl(0, "Pmenu", { bg = "none" })
 hl(0, "PmenuSbar", { bg = "none" })
-hl(0, "PmenuSel", { fg = yellow, bg = bg })
-
+hl(0, "PmenuSel", selection)
 hl(0, "PmenuThumb", { bg = "none" })
+
 hl(0, "BlinkCmpLabel", { link = "Identifier" })
 hl(0, "BlinkCmpLabelDetail", { link = "Comment" })
 hl(0, "BlinkCmpLabelDescription", { link = "Comment" })
